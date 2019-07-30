cd1fea18dfc71f1da4caa82a460c27bc91bc3336
sgupta9591 <sgupta9591@gmail.com>
2019-07-30 22:38:41 +0530

    server

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require("fs");
+ 
+http.createServer(
+	function(request, response) {
+		fs.readFile("index.html", function(err, data){
+		  	response.writeHead(200, {'Content-Type': 'text/html'});
+		  	response.write(data);
+		  	response.end();
+		});
+	}
+).listen(process.env.PORT || 8080);
