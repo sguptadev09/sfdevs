f76e608790d8f0121b2844f1c36d1b5010aece84
sgupta9591 <sgupta9591@gmail.com>
2019-09-15 09:33:16 +0530

    index

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var http = require('http');
+var https = require('https');
 var fs = require("fs");
+
+const url = 'https://sf-devs-developer-edition.ap15.force.com/';
  
 http.createServer(
 	function(request, response) {
-		fs.readFile("index.html", function(err, data){
-		  	response.writeHead(200, {'Content-Type': 'text/html'});
-		  	response.write(data);
-		  	response.end();
+		https.get(url, function(res) {
+			let html = '';
+			res.on('data', (d) => {
+				html += html + d;
+			});
+			res.on('end', () => {
+				response.writeHead(200, {'Content-Type': 'text/html'});
+			  	response.write(html);
+			  	response.end();
+			})
 		});
 	}
 ).listen(process.env.PORT || 8080);
